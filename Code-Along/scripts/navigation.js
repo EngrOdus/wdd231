8a0dc68cd0c09da191a1b19c4b3cf3bcb9ccfb24
EngrOdus <henry.odumamwen@outlook.com>
2025-10-29 21:01:26 +0100

    Developed the small-view.css, large.css and navigation.js files for responsive navigation bar.

--- Code-Along/scripts/navigation.js
+++ Code-Along/scripts/navigation.js
@@ -1,5 +1,7 @@
 const hamburgerBtn = document.getElementById("ham-btn");
+const navBar = document.querySelector("#nav-bar");
 
 hamburgerBtn.addEventListener("click", () => {
     hamburgerBtn.classList.toggle("show");
+    navBar.classList.toggle("show");
 })
