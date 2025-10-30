f7b7fac6bbc404dfa810c9b064e036449c2bcd55
EngrOdus <henry.odumamwen@outlook.com>
2025-10-30 16:12:38 +0100

    Developed lastVisit.js, navigation.js, lastModified.js and chamber-commerce.css files.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,7 @@
 const hamburgerBtn = document.getElementById("ham-btn");
+const navBtn = document.getElementById("nav-bar");
 
 hamburgerBtn.addEventListener("click", () => {
     hamburgerBtn.classList.toggle("show");
+    navBtn.classList.toggle("show");
 })
